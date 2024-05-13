e11bf7944e2a749e7824f37058098726458a3b43
tegarnurwidianto <tnur1583@gmail.com>
2024-05-13 11:24:34 +0700

    cek

---
+++ project/resources/views/view_home.blade.php
@@ -0,0 +1,24 @@
+@extends('index')
+@section('title', 'Home')
+
+@section('isihalaman')
+    <h3>Pengertian perpustakaan menurut para ahli</h3>
+    <p>
+    <h4>Perpustakaan</h4>
+    Menurut UU Perpustakaan pada Bab I pasal 1 menyatakan Perpustakaan adalah institusi yang mengumpulkan pengetahuan tercetak dan terekam, mengelolanya dengan cara khusus guna memenuhi kebutuhan intelektualitas para penggunanya melalui beragam cara interaksi pengetahuan.
+    <p>
+    <h4>
+    SEJARAH SINGKAT DINAS PERPUSTAKAAN DAN KEARSIPAN KABUPATEN BANGKALAN</h4>
+<p>
+    Perjalanan status pengelolaan perpustakaan dan arsip melalui proses tahapan panjang. Pada awalnya, perpustakaan Kabupaten Bangkalan berada di Bagian Umum Sekretariat Daerah Kabupaten Bangkalan, pada Sub Bagian Sosial dan Kearsipan. Dengan ditetapkan Perda Kabupaten Bangkalan No. 26 Tahun 2000 tentang Organisasi dan Tata Kerja Lembaga Teknis Daerah, secara resmi menjadi Kantor Arsip dan Perpustakaan, yang lokasinya terletak di Jl. R.A Kartini No. 1 Bangkalan.
+
+Pada tahun 2008 dengan adanya perubahan ketentuan yang diatur dalam Perda Kabupaten Bangkalan No. 4 Tahun 2008, nomenklaturnya berubah menjadi Kantor Perpustakaan Arsip dan Dokumentasi.
+
+Pada tahap keempat, perubahan terjadi lebih signifikan, karena yang semula statusnya berupa kantor menjadi dinas sebagaimana diatur dalam Perda Kabupaten Bangkalan No. 7 Tahun 2016 tentang Pembentukan dan Susunan Perangkat Daerah, maka Kantor Perpustakaan, Arsip dan Dokumentasi menjadi Dinas Perpustakaan dan Kearsipan, yang sekarang yang menjadi Kepala Dinasnya adalah H. Moch. Musleh, SH. MH,. Dinas Perpustakaan dan Kearsipan memiliki 3 gedung, yakni Kantor Dinas dan Depo Arsip di Jl. Halim Perdana Kusuma No. 2 dan Perpustakaan Umum di Jl. Ra. Kartini No. 1 Bangkalan.
+</p>
+
+    <h4>Perpustakaan Menurut UU NO 43. THN. 2007</h4>
+    Perpustakaan adalah institusi yang mengumpulkan pengetahuan tercetak dan terekam, mengelolanya dengan cara khusus guna
+    memenuhi kebutuhan intelektualitas para penggunanya melalui berbagai cara interaksi pengetahuan.
+
+@endsection
